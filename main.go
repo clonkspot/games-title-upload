bc55db571d84d77d4c1e55c7dc05e7b6535a07d3
Luchs <Luchs@euirc.eu>
2014-12-12 20:47:39 +0100

    Validate the uploaded file to be a small PNG

--- main.go
+++ main.go
@@ -6,10 +6,36 @@ import (
 	"io"
 	"log"
 	"fmt"
+	"bytes"
+	"image/png"
 	"path/filepath"
 )
 
+const filesizeLimit = 100 * 1024
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	// Check that the body is a PNG file with a maximum of 100KB.
+	var buf bytes.Buffer
+
+	n, err := buf.ReadFrom(io.LimitReader(r.Body, filesizeLimit))
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, err)
+		return
+	}
+	if n >= filesizeLimit {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "File too large.")
+		return
+	}
+
+	_, err = png.Decode(bytes.NewReader(buf.Bytes()))
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "File not a PNG image.")
+		return
+	}
+
 	path := "." + r.URL.Path
 	os.MkdirAll(filepath.Dir(path), 0666)
 
@@ -22,7 +48,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	defer out.Close()
 
 	// write the content from POST to the file
-	written, err := io.Copy(out, r.Body)
+	_, err = io.Copy(out, &buf)
 	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintln(w, err)
