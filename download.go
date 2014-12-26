a9b14328ef8fe7323d8d0f698097ab716a5f469b
Luchs <Luchs@euirc.eu>
2014-12-26 18:29:46 +0100

    Add download handler
    The download handler will try the working directory first, then the
    download directory.

---
+++ download.go
@@ -0,0 +1,32 @@
+/* Title.png downloads */
+
+package main
+
+import (
+	"net/http"
+	"os"
+	"fmt"
+)
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	path := "." + r.URL.Path
+	if fileExists(path) {
+		servePath(w, path)
+	} else if fileExists(uploadPrefix + path) {
+		servePath(w, uploadPrefix + path)
+	} else {
+		w.WriteHeader(404)
+	}
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	fmt.Println(path, err)
+	return err == nil
+}
+
+// Instructs nginx to serve the given path.
+func servePath(w http.ResponseWriter, path string) {
+	w.Header().Add("X-Accel-Redirect", internalPrefix + path)
+	w.WriteHeader(200)
+}
--- main.go
+++ main.go
@@ -6,8 +6,24 @@ import (
 	"os"
 )
 
-func main() {
-	http.HandleFunc("/", uploadHandler)
+// The prefix which will be used in X-Accel-Redirect.
+var internalPrefix = "/serve/"
+// The upload directory.
+var uploadPrefix = "./incoming/"
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			downloadHandler(w, r)
+		case "POST":
+			uploadHandler(w, r)
+		default:
+			w.WriteHeader(400)
+		}
+	})
+
+	port := os.Getenv("PORT")
+	log.Print("Listening on port "+port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
